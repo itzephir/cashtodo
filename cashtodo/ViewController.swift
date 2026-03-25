efad227a3afe5eebe6f2721807850ca2d14156ee
itzephir <d.y.dvoryannikov@gmail.com>
2026-03-25 07:44:07 +0300

    Initial Commit

---
+++ cashtodo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cashtodo
+//
+//  Created by Dmitrii Dvoriannikov on 25.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
